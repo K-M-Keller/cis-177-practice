f8715832fd9111f97eb580ca138fcb5519aa40c1
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-10 13:47:10 -0600

    notes 2-10

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-// Underfinded is a PRIMITIVE TYPE.
-// It signifies the absence of ANY value.
-let x;
+// Dot notation
+// const me = {
+//   name: "Katy",
+//   age: 36,
+//   pets: {
+//     pet1: "Ellie",
+//     pet2: "Demon"
+//   },
+//   shows: "Sabrina"
+// };
 
-x = 23;
-x = 0;
-// NULL is a PRIMITIVE.
-// NULL represents a deliberate assignment of 'nothing'
-console.log(x);
+// const keyThatINeed = "pets";
+// console.log(me[keyThatINeed]);
 
-// This is an OBJECT.
-const me = {
-  name: "Katy",
-  age: 36,
-  pets: {
-    cat: "Demon",
-    dog: "Ellie"
-  }
-};
+// Bracket notation
+// Use BRACKET NOTATION to access a key that is REFERENCED by a VARIABLE.
+//console.log(me["pets"]);
 
-// Create an new key value pair with age = 23.
-me.age = 23;
-me.name = "Keller";
+//console.log("Bracket notation with variable: ", me.pets[keyThatINeed]);
 
-console.log(me.pets.cat);
+// Relational or COMPARISON OPERATORS always yields BOOLEANS.
+// console.log(2 === 2);
+
+// To check if 2 things are considered to equal...
+// '===' is STRICT EQUALITY
+// It checks the value and TYPE.
+// console.log(2 === "2"); //false
+// console.log(2 === 3);
+// console.log(2 !== 3); //true
+
+// const x = 3;
+
+// EXPRESSION is anything that evaluates a value
+// STATEMENTS combine EXPRESSIONS with keyboards and other syntax
+// {}s establish the scope of the code
+// if (x === 3) {
+//   console.log("x is 3!");
+// } else {
+//   console.log("x is not 3!");
+// }
+// const age = 20;
+
+// if (age >= 25) {
+//   console.log("Renting a car is affordable");
+// } else if (age >= 18) {
+//   console.log("Renting a car is expensive");
+// } else {
+//   console.log("You cannot rent");
+// }
+
+// JS is a DYNAMICALLY TYPED language
+const name = "";
+// We can quickly check to see if  value is "truthy" or "falsey"
+// For examples, if I left the 'name' blank on a form
+if (name) {
+  console.log("the user filled out the name");
+} else {
+  console.error("user left name blank");
+}
