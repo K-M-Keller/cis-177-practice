f07ee7ad08a1f568d6614c77af9026fc8aab428a
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-17 13:15:43 -0600

    example of logical operators

--- index2.js
+++ index2.js
@@ -1,33 +1,11 @@
-const name = "katy";
-let num = 1;
+const x = 3;
 
-// console.log(
-//   "my name is " + name + " and I have been coding for " + num + " years."
-// );
+// TODO: Check if x is ODD AND if it's less than 10
+if (x % 2 === 1 && x < 10) {
+  console.log("x is odd if it has a remainder of 1.");
+}
 
-// // Template Literals and Interpolation
-
-// console.log(`My name is ${name}`);
-
-// // Create a variable 'timeOfDay' and assign it to a string
-
-// const timeOfDay = "Morning";
-
-// console.log(`Good ${timeOfDay}`);
-// console.log(typeof timeOfDay); //show what type of data it is
-
-// // JS will COERCE your data types into whatever it feels appropriate.
-// // IMPLICIT
-// console.log("22" + 22);
-// console.log(22 + 22);
-// console.log(22 + true);
-// console.log(22 + false);
-
-// EXPLICIT COERCION you tell JS what/how you want it done
-console.log(Number(false));
-
-console.log(parseInt("123.45")); //Shows before the decimal.
-console.log(parseFloat("123.45")); //Shows the entire number.
-
-console.log(Boolean(222));
-console.log("3" * 3);
+// TODO: Check if x is ODD OR if it's less than 10
+if (x % 2 === 1 || x < 10) {
+  console.log("x is odd if it has a remainder of 1.");
+}
