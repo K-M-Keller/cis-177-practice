3a42348d20d5c08e34e39dadf5347eb680bbadf7
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-19 13:32:38 -0600

    fizzbuzz

--- index2.js
+++ index2.js
@@ -1,8 +1,56 @@
-// TODO: Log the numbers from 1 to 20
-let x = 1;
+// TODO: Use a WHILE to log the numbers from 1 to 100.
+// However, only log the odd numbers.
+// HINT: Use an 'if' inside the while nd use:
+// i % 2 === 1 to determine if its odd.
 
-while (x <= 10) {
-  console.log(x);
-  // Alway make sure there is and exit condition.
-  x += 1; // x = x + 1;
-}
+// let i = 0;
+
+// while (i <= 100) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+//   i += 1;
+// }
+
+// TODO: Sum the numbers from 1 to 10.
+
+// let x = 0;
+// let total = 0;
+
+// while (x <= 10);
+// {
+//   total += 1; //shorthand for total = total + x, also total +=x;
+// }
+// console.log(total);
+
+// TODO: FIzz Buzz
+// Log the numbers from 1 to 100.
+// However if the number is divisible by 3, log fizz.
+// However if the number is divisible by 5, log buzz.
+// However if the number is divisible by both, log fizz buzz.
+
+// let i = 1;
+
+// while (i <= 100) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz!");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz!");
+//   }else {
+// console.log(i);
+// }
+// i += 1;
+
+// '!' is the NOT operator.
+// Using this in front of any data type (such as a number)
+// will immediately convert that data type into a Boolean.
+// Then, that avlue will be inverted as per the NOT operator.
+
+console.log(Boolean(0));
+console.log(!0);
+console.log(!"Hello");
+console.log(!"");
+console.log(!true);
+console.log(!!true);
--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-let x = 3;
+// TODO: FIzz Buzz
+// Log the numbers from 1 to 100.
+// However if the number is divisible by 3, log fizz.
+// However if the number is divisible by 5, log buzz.
+// However if the number is divisible by both, log fizz buzz.
 
-while (x <= 15) {
-  console.log(x);
-  // Alway make sure there is and exit condition.
-  x += 5; // x = x + 1;
+let i = 1;
+
+while (i <= 100) {
+  if (!(i % 3) && !(i % 5)) {
+    console.log("FizzBuzz");
+  } else if (!(i % 3)) {
+    console.log("Fizz!");
+  } else if (!(i % 5)) {
+    console.log("Buzz!");
+  } else {
+    console.log(i);
+  }
+  i += 1;
 }
