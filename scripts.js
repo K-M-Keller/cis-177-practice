549ef84a19b31ee02c5400f912e2dc5042e13169
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-19 07:51:59 -0600

    homework

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
-const timeOfDay = "Night";
-console.log(`Good ${timeOfDay}`);
+let x = 3;
+
+while (x <= 15) {
+  console.log(x);
+  // Alway make sure there is and exit condition.
+  x += 5; // x = x + 1;
+}
