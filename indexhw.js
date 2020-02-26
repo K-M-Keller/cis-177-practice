95f8b858c7467694e2555082bd3d71e82802591a
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-26 09:48:07 -0600

    homework

--- indexhw.js
+++ indexhw.js
@@ -1,2 +1,11 @@
-for (let i = 0; i <= 100; )
-  console.log((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
+for (let i = 1; i <= 100; ) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz!");
+  } else if (i % 5 === 0) {
+    console.log("Buzz!");
+  } else {
+    console.log(i);
+  }
+}
