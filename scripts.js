9cbf4cab2a1940b78542ad0a9020062232719449
Katy Keller <kathryn.keller@student.swic.edu>
2020-02-26 13:32:18 -0600

    prompt while blank

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-const user = {};
+// IMPLICIT CONVERSATION
+// || means OR
+// If first expression is "truthy" then we do not EVALUATE the right hand-side.
+// If prompt RETURNS an empty STRING - b/c of the '||' this will be IMPLICITLY CONVERTED to 'false'
 
-// The result of CONFIRM (T or F)is assigned to the value associated with user
-// We will mutate the object based on confirm.
-user.isHappy = confirm("Are you happy?");
+// 'name will be a STRING, but it might be blank.
+let name = prompt("What is your name?");
 
-console.log(user);
+// If 'name' is blank, then '!' will convert to a Boolean (NOT FALSE)
+// If the name is not blank, then '!' will convert it to a Boolean (NOT TRUE)
+while (!name) {
+  name = prompt("What is your name?");
+}
+console.log(name);
